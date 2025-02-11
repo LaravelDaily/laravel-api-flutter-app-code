fd2994f5703adc469435a9aa0d80b97e3ca7a8f0
Modestas Vaitkevičius <info@modestasv.lt>
2025-02-11 10:56:13 +0200

    Added categories edit form

--- lib/screens/categories/categories_list.dart
+++ lib/screens/categories/categories_list.dart
@@ -22,6 +22,9 @@ class CategoriesList extends StatefulWidget {
 
 class CategoriesListState extends State<CategoriesList> {
   Future<List<Category>>? futureCategories;
+  final _formKey = GlobalKey<FormState>();
+  late Category selectedCategory;
+  final categoryNameController = TextEditingController();
 
   Future<List<Category>> fetchCategories() async {
     final http.Response response = await http.get(
@@ -36,6 +39,28 @@ class CategoriesListState extends State<CategoriesList> {
     List categories = data['data'];
 
     return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  Future saveCategory() async {
+    final form = _formKey.currentState;
+
+    if (!form!.validate()) {
+      return;
+    }
+
+    String url =
+        'https://78ac-78-58-236-130.ngrok-free.app/api/categories/${selectedCategory.id}';
+    final http.Response response = await http.put(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': categoryNameController.text,
+      }),
+    );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -60,6 +85,48 @@ class CategoriesListState extends State<CategoriesList> {
                     Category category = snapshot.data![index];
                     return ListTile(
                       title: Text(category.name),
+                      trailing: IconButton(
+                          onPressed: () {
+                            selectedCategory = category;
+                            categoryNameController.text = category.name;
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    padding: EdgeInsets.all(20),
+                                    child: Form(
+                                      key: _formKey,
+                                      child: Column(
+                                        children: [
+                                          Text('Edit Category'),
+                                          TextFormField(
+                                            decoration: InputDecoration(
+                                              labelText: 'Category Name',
+                                            ),
+                                            controller: categoryNameController,
+                                            validator: (String? value) {
+                                              if (value == null || value.isEmpty) {
+                                                return 'Please enter category name';
+                                              }
+                                              return null;
+                                            },
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(top: 20),
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                saveCategory();
+                                              },
+                                              child: Text('Update'),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                });
+                          },
+                          icon: Icon(Icons.edit)),
                     );
                   });
             } else if (snapshot.hasError) {
